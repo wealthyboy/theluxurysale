d5e84f1d0c5afd2375619332ca204520f094f952
Jacob Atam <jacob.atam@gmail.com>
2020-12-22 18:30:58 +0100

    Updated nav

--- resources/js/components/navigation/NavIcon.vue
+++ resources/js/components/navigation/NavIcon.vue
@@ -4,10 +4,13 @@
         <div class="header-contact d-none d-lg-flex align-items-center ml-auto pr-xl-4 mr-4">
         </div><!-- End .header-contact -->
         
-        <a  v-if="!$root.loggedIn"  href="/login" class="header-icon  pl-1">         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M14.85 14.282a6 6 0 10-5.699 0C5.962 14.867 3 16.23 3 17v4h18v-4c0-.77-2.962-2.132-6.15-2.718zM16 9a4 4 0 11-8 0 4 4 0 018 0zm3 8.748V19H5v-1.252c.377-.218.947-.48 1.673-.74C8.269 16.438 10.287 16 12 16s3.73.438 5.327 1.009c.726.259 1.296.521 1.673.74z"></path></svg>
-</a>
+        <a  v-if="!$root.loggedIn"  href="/login" class="header-icon  pl-1">         
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M14.85 14.282a6 6 0 10-5.699 0C5.962 14.867 3 16.23 3 17v4h18v-4c0-.77-2.962-2.132-6.15-2.718zM16 9a4 4 0 11-8 0 4 4 0 018 0zm3 8.748V19H5v-1.252c.377-.218.947-.48 1.673-.74C8.269 16.438 10.287 16 12 16s3.73.438 5.327 1.009c.726.259 1.296.521 1.673.74z"></path></svg>
+        </a>
         <div v-if="$root.loggedIn" class="header-dropdown ml-4">
-            <a href="/account" class="header-icon  pl-1"><i class="icon-user-2"></i></a>
+            <a href="/account" class="header-icon  pl-1">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M14.85 14.282a6 6 0 10-5.699 0C5.962 14.867 3 16.23 3 17v4h18v-4c0-.77-2.962-2.132-6.15-2.718zM16 9a4 4 0 11-8 0 4 4 0 018 0zm3 8.748V19H5v-1.252c.377-.218.947-.48 1.673-.74C8.269 16.438 10.287 16 12 16s3.73.438 5.327 1.009c.726.259 1.296.521 1.673.74z"></path></svg>
+            </a>
 
             <div class="header-menu ">
                 <ul>  
