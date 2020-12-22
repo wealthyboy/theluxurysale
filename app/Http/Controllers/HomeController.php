ea9c28bb5c043df81a4764195d3b4d0f223127c1
Jacob Atam <jacob.atam@gmail.com>
2020-12-22 13:47:10 +0100

    Removed art filter

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,7 +29,7 @@ class HomeController extends Controller
         $products = Product::where('featured',1)->orderBy('created_at','DESC')->take(8)->get();
         $posts  =   Information::orderBy('created_at','DESC')->where('blog',true)->take(3)->get();
         $page_title = 'The Luxury sale - Verified Authentic ';
-        return view('underconstruction.index');
+        return view('index',compact('page_title','products','posts'));
 
 
             
