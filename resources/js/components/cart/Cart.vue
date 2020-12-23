5af66e014731f8b32eae545794a25e633de1517e
Jacob Atam <jacob.atam@gmail.com>
2020-12-23 15:26:48 +0100

    Updated contact

--- resources/js/components/cart/Cart.vue
+++ resources/js/components/cart/Cart.vue
@@ -1,99 +1,85 @@
 <template>
    <div>
-        
-        <div v-if="loading && !carts.length" class="row">
-             <div class="col-md-8">
-                <article>
-                    <div class="cart-product-table-wrap">
-                        <div    class="row cart-rows raised mb-4 pb-3 pt-3 mt-3 border border-gray">
-                            <div class="col-md-7 col-6">
-                                <span  class='spinner-border spinner-border-sm' role='status' aria-hidden='true'></span> Loading....
-                            </div>
-                        </div>
-                    </div>
-                </article>
-            </div>
-        </div>
-
         <div v-if="!loading && carts.length" class="row">
+            
             <div class="col-md-8">
                 <article>
                     <form action="/update/cart" method="" class="cart-form">
                         <input type="hidden" value="" name="_token" />
-                        <div class="cart-product-table-wrap ">
-                                <div v-for="cart in carts"  :key="cart.id" class="row cart-rows raised mb-3 pt-4 pb-4 border border-gray">
-                                    <div class="col-md-2 col-6">
-                                        <div class="cart-image">
-                                            <img :src="cart.product_variation.image_tn" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="col-md-7 col-6">
-                                        <h5><a href="#">{{ cart.product_name }}</a></h5>
-                                        <div class="product--share  mt-3">
-                                            <span class="bold">Item #:</span> {{ cart.product_variation.sku }}
-                                        </div>
-                                         
-                                              <!--Product Ratting-->
-                                        <div class="product-item-price"  v-if="cart.product_variation.discounted_price">
-                                            <div class="product-price-amount">
-                                                <span class="retail-title text-gold">SALE PRICE</span>
-                                                <span class="product--price text-gold">{{ meta.currency }}{{ cart.product_variation.discounted_price | priceFormat }}</span>
-                                                <span class="retail-title">{{ cart.product_variation.percentage_off }}% off</span>
-                                            </div>
-
-                                            <div class="product-price-amount retail">
-                                                <span class="retail-title text-gold">PRICE</span>
-                                                <span class="product--price retail-price text-gold">{{ meta.currency }}{{ cart.product_variation.price | priceFormat }}</span>
-                                                <span class="retail-title"></span>
-                                            </div>
-                                        </div>
-
-                                        <div class="product-item-price" v-else>
-                                            <div class="product-price-amount">
-                                                <span class="retail-title text-gold">PRICE</span>
-                                                <span class="product--price">{{ meta.currency }}{{ cart.price | priceFormat}}</span>
-                                                <span class="retail-title"></span>
-                                            </div>
-                                        </div>
-                                        <p v-if="cart.variations.length"> {{ cart.variations.toString() }}</p>
-                                    </div>
-                                    <div class="col-md-2">
-                                        <div class="pt-2 pb-4">                     
-                                            <label class="bold">Qty</label>
-                                            <div id="quantity_1234" class="product-quantity">
-                                                <select @change="updateCartQty($event,cart.product_variation.id)" id="add-to-cart-quantity" name="qty"  class="input--lg form-full"> 
-                                                    <option   v-for="x in parseInt(cart.product_variation.quantity)" :selected="x == cart.quantity"  :key="x" >{{ x }}</option>
-                                                </select> 
-                                            </div>
-                                        </div>
-                                        <div class="">                     
-                                            <a href="#"  @click.prevent="removeFromCart($event,cart.id)" class=" text-danger btn-transparent bold"> 
-                                                <span class="button-icon"> 
-                                                    <i class="far fa-trash-alt"></i>
-                                                </span>
-                                                {{ removeCart }} 
-                                            </a> 
-                                        </div>
+                        <div class="cart-product-table-wrap  bg--light">
+                            <div v-for="cart in carts"  :key="cart.id" class="row cart-rows raised mb-3 pt-4 pb-4 border border-gray">
+                                <div class="col-md-2 col-6">
+                                    <div class="cart-image">
+                                        <img :src="cart.product_variation.image_tn" alt="">
                                     </div>
                                 </div>
+                                <div class="col-md-7 col-6">
+                                    <h5><a href="#">{{ cart.product_name }}</a></h5>
+                                    <div class="product--share">
+                                        <span class="bold">Item #:</span> {{ cart.product_variation.sku }}
+                                    </div>
+                                    
+                                    <!--Product Ratting-->
+                                    <div class="product-item-price"  v-if="cart.product_variation.discounted_price">
+                                        <div class="product-price-amount">
+                                            <span class="retail-title text-gold">SALE PRICE</span>
+                                            <span class="product--price text-gold">{{ meta.currency }}{{ cart.product_variation.discounted_price | priceFormat }}</span>
+                                            <span class="retail-title">{{ cart.product_variation.percentage_off }}% off</span>
+                                        </div>
+
+                                        <div class="product-price-amount retail">
+                                            <span class="retail-title text-gold">PRICE</span>
+                                            <span class="product--price retail-price text-gold">{{ meta.currency }}{{ cart.product_variation.price | priceFormat }}</span>
+                                            <span class="retail-title"></span>
+                                        </div>
+                                    </div>
+
+                                    <div class="product-item-price" v-else>
+                                        <div class="product-price-amount">
+                                            <span class="retail-title text-gold">PRICE</span>
+                                            <span class="product--price">{{ meta.currency }}{{ cart.price | priceFormat}}</span>
+                                            <span class="retail-title"></span>
+                                        </div>
+                                    </div>
+                                    <p v-if="cart.variations.length"> {{ cart.variations.toString() }}</p>
+                                </div>
+                                <div class="col-md-2">
+                                    <div class="pt-2 pb-4 form-group">                     
+                                        <label class="bold">Qty</label>
+                                        <div id="quantity_1234" class="product-quantity select-custom">
+                                            <select @change="updateCartQty($event,cart.product_variation.id)" id="add-to-cart-quantity" name="qty"  class="input--lg form-control"> 
+                                                <option   v-for="x in parseInt(cart.product_variation.quantity)" :selected="x == cart.quantity"  :key="x" >{{ x }}</option>
+                                            </select> 
+                                        </div>
+                                    </div>
+                                    <div class="">                     
+                                        <a href="#"  @click.prevent="removeFromCart($event,cart.id)" class=" text-danger btn-transparent bold"> 
+                                            <span class="button-icon"> 
+                                                <i class="far fa-trash-alt"></i>
+                                            </span>
+                                            {{ removeCart }} 
+                                        </a> 
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </form>
                 </article>
             </div>
 
-            <div class="col-md-4">
-                <div class="cart-collateralse  border pb-3 pt-3 pl-3 pt-3 pr-3 raised">
+            <div class="col-md-4 ">
+                <div class="cart-collateralse  bg--light  border pb-3 pt-3 pl-3 pt-3 pr-3 raised">
                     <div class="cart_totalse">
                         <h3> Summary </h3>
-                        <p class="">
-                        <span class="bold">Subtotal</span> 
-                        <span class="price-amount amount bold pull-right"><span class="currencySymbol">{{  meta.currency }}{{ meta.sub_total | priceFormat}}</span></span>
-                        </p>
-                        <hr/>
-                        <p>
+                        <div class="p-3 border-bottom">
+                        <span class="bold ">Subtotal</span> 
+                        <span class="price-amount amount bold float-right"><span class="currencySymbol">{{  meta.currency }}{{ meta.sub_total | priceFormat}}</span></span>
+                        </div>
+                        
+                        <div class="p-3">
                         <span class="bold">Total</span> 
-                            <span class="price-amount amount bold pull-right"><span class="currencySymbol">{{  meta.currency }}{{ meta.sub_total | priceFormat}}</span></span>
-                        </p>
+                            <span class="price-amount amount bold float-right"><span class="currencySymbol">{{  meta.currency }}{{ meta.sub_total | priceFormat}}</span></span>
+                        </div>
                         <div class="proceed-to-checkout">
                             <a href="/checkout" class="checkout-button btn btn--lg btn--primary bold full-width">Proceed to checkout</a>
                         </div>
@@ -103,19 +89,22 @@
             <!--End Paragraph-->
         </div>
         
-        <div v-if="!loading && !carts.length">
-            <section class="sec-padding-b">
-                <div class="container">
-                    <p class="lead">Your cart is empty </p>
-                    <p class="bold"><a href="/">Continue shopping >>></a> </p>
+        <div class="row justify-content-center" v-if="!loading && !carts.length">
+            <div class="col-md-10">
+                <div class="error-page text-center">
+                    <h1>Your Cart Is Empty</h1>
+                    <p class="large">You don't have any items in your cart</p>
+                    <a href="/" class="btn border">Continue Shopping</a>
                 </div>
-            </section>
+            </div> 
         </div>
     </div>
 </template>
-<script>
+
+<<script>
 
 import  { mapGetters,mapActions } from 'vuex'
+
 
 export default {
     data(){
@@ -123,6 +112,7 @@ export default {
            removeCart: 'Remove',
         }
     },
+    
     computed: {
         ...mapGetters({
             carts: 'carts',
@@ -130,8 +120,7 @@ export default {
             loading:'loading'
         })
     },
-    mounted(){
-    },
+   
     methods: {
         ...mapActions({
             getCart:'getCart',
--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -1,33 +1,65 @@
+ 
+  
+
+
 @extends('layouts.app')
  
-@section('content')
-@include('_partials.home.intro2')
-
+ @section('content')
   
-   <!--Content-->
-    <section class="pb-4 mt-1">                
-        <div class="container">
-            <div class="row cart-header mt-4 mb-1 pb-1">
-                <div class="col-lg-12 col-md-12 col-12">
-                    <h3> Your Cart</h3> 
-                </div>
-            </div>
-            @if ($carts->count())
-               <cart-summary :sub_total="{{ $sub_total }}" :crts="{{ $carts }}" />
-            @else
-                <div class="row">
-                    <section class="sec-padding-b">
-                        <div class="container">
-                            <p class="lead">Your cart is empty </p>
-                            <p class=""><a href="/">Continue shopping >>></a> </p>
-                        </div>
-                    </section>
-                </div>
-            @endif
-        </div>
-    </section>
-    <!--End Content-->
-@endsection
-
-
-
+ <div class="top-notice text-white bg--primary cart-page">
+     <div class="container-fluid text-center">
+         <div class="row">
+             
+             <div class="col-12">
+                 <h5 class="d-inline-block color--light text-uppercase  mb-0"><b><i class="fas fa-money-check"></i>
+                 USE TLSXMAS and get 10% off when you shop over 250k </b>
+                 </h5>
+             </div>
+             
+ 
+         </div>
+         
+     </div><!-- End .container -->
+ </div>
+ 
+ <section class="breadcrumb no-banner cart-page justify-content-center">
+     <div class="breadcrumb-content">
+         <div class="container-fluid">
+             <div class="row">
+                 <div class="col-12  text-center border-bottom">
+                     <h1 class="breadcrumb-title">Your Cart</h1>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
+   
+    <!--Content-->
+    
+ <section class="bg--gray">          
+     <div class="container-fluid">
+         <div id="js-loading"  class="full-bg">
+             <div class="signup--middle">
+                 <div class="loading">
+                     <div class="loader"></div>
+                 </div>
+                 <img src="{{ $system_settings->logo_path() }}" height="110" width="80" alt="The Luxury sale Logo">
+             </div>        
+         </div>
+         <cart-summary  />
+ 
+     </div>
+ </section>
+     <!--End Content-->
+ @endsection
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
