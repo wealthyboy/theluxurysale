feb08d876d3c241f4901764ddc0d2c29e6c29fc3
Jacob Atam <jacob.atam@gmail.com>
2021-01-13 18:22:20 +0100

    Added new banner link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,8 +20,8 @@
     <div class="row">
         <div class="col-md-12  col-12 ">
             <div class="ba">
-                <a class="portfolio-interior" href="https://theluxurysale.com/products/women-designers-giuseppe-zanotti">
-                    <img src="https://theluxurysale.com/uploads/H4XRixYFY4GWDcWq5zGDsIJP3wARY1bgM05H2X6O.jpg" alt="12 percent off" />
+                <a class="portfolio-interior" href="https://theluxurysale.com/products/brand-of-the-week">
+                    <img src="https://theluxurysale.com/uploads/nyhYobKcfd4GekLFopYntBHhDCQj3cC3X6C1hwBQ.gif" alt="12 percent off" />
                 </a>
             </div>
         </div>
