1b28014b039f2f14c3e9e242298ab49af1967919
Jacob Atam <jacob.atam@gmail.com>
2021-01-17 12:06:20 +0100

    Renamed migration

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,7 +37,7 @@ class HomeController extends Controller
             return view('index',compact('page_title','products','posts','banners','sliders')); 
         } else {
             
-            return view('underconstruction.index');
+            return view('index',compact('page_title','products','posts','banners','sliders')); 
         }
     }
 
