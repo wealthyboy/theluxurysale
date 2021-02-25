271d5721dea8126b4764855158813bb3134df8cc
Atam Jacob <jacob.atam@gmail.com>
2021-02-25 03:57:09 -0800

    Updated web hook

--- resources/js/components/checkout/CheckoutIndex.vue
+++ resources/js/components/checkout/CheckoutIndex.vue
@@ -376,6 +376,7 @@ export default {
             this.order_text =  'Please wait. We are almost done......'
             this.payment_is_processing =true
             this.payment_method ='card'
+            console.log(context.coupon)
                 FlutterwaveCheckout({
                     public_key: "FLWPUBK_TEST-dcd3315c964f59ff4c04fa1b0eea9595-X",
                     tx_ref: "rave-"+ Math.floor((Math.random() * 1000000000) + 1), 
