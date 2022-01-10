8ba1457c04ec8ad9cc4f40633e8cabfa8ec78703
Atam Jacob <jacob.atam@gmail.com>
2022-01-09 23:28:00 -0800

    Updated ui

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -59,7 +59,7 @@
                 </div>
             </div>
 
-            <div class="{{ $banner->col }} {{ $banner->sm_col_width }} {{ $banner->col == 'col-lg-3' ?  'col-6    p-0' : '' }} p-1 {{ $banner->title }} text-left  d-block d-sm-none">
+            <div class="{{ $banner->col }} {{ $banner->sm_col_width }} {{ $banner->col == 'col-lg-3' ?  'col-6    p-0' : '' }} p-1 {{ $banner->title }} text-left  d-block d-sm-none mb-3">
                 <div class="bg-left-panel ml-5 mr-5">
                     <h1 class="bold">{{ $banner->title }}</h1>
                     <?php echo  html_entity_decode($banner->description);  ?> 
