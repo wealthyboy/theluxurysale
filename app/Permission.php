1a394c942c5bf2c61f7f7a94d5b068688899c001
Seth Atam <seth.atam@gmail.com>
2023-01-07 20:33:46 +0100

    U

--- app/Permission.php
+++ app/Permission.php
@@ -11,7 +11,18 @@ class Permission extends Model
     //
     use SoftDeletes;
 
-    protected $fillable =['code','name'];
+    protected $fillable = ['code', 'name'];
 
-    
+    public static $types = [
+        'Account' => 1,
+        'Create' => 2,
+        'Read' => 3,
+        'Update' => 4,
+        'Delete' => 5,
+        'Reports' => 6,
+        'Add Admin Users' => 7,
+        'Activity' => 8,
+        'permissions' => 10,
+        'Enable/Disble Site' => 9
+    ];
 }
