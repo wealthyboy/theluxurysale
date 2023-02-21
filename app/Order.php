8be8792b160e0f361fca1f059f6b984b68bf5d25
Jacob Atam <jacobatam@Jacobs-MacBook-Pro.local>
2023-02-21 03:49:48 -0800

    U

--- app/Order.php
+++ app/Order.php
@@ -92,7 +92,6 @@ class Order extends Model
 				"Email" => $order->email,
 				"Type" => $order->order_type,
 				"Payment Type" => $order->payment_type,
-				"Status" => $order->status,
 				"Agent" => null !== $order->user ? $order->user->fullname() : $order->fullName(),
 				"Total" => $order->currency . number_format($order->total),
 				"Date Added" => $order->created_at->format('d-m-y'),
